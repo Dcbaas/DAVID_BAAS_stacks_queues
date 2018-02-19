852b00ac7d38c1ff30ea883a716c2f40f4ced905
Dcbaas <baasdav@eos01.cis.gvsu.edu>
2018-02-18 19:07:09 -0500

    Added balancedSymbol Test for a file

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
 #include "functions.h"
 #include <string>
-
+#include <fstream>
 
 TEST_CASE("Test BalancedSymbols", "[inner]"){
 	std::string input = "()()(abc){}";
@@ -15,4 +14,19 @@ TEST_CASE("Test Invalid Input BalSymbols", "[inner]"){
 	std::string input = "{{})(((";
 
 	CHECK_FALSE(balancedSymbols(input) == true);
+
+}
+//Test a java src file.
+TEST_CASE("Test a file", "[inner]"){
+	std::ifstream in{"TestText.txt"};
+	std::string input = "Test";
+	CHECK(input == "Test");		
+	if(not in)
+		std::perror("TestText.txt");
+	else{
+		in >> input;
+	}
+
+	CHECK_FALSE(input == "Test" );
+	CHECK(balancedSymbols(input) == true);
 }
