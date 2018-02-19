4c974ddb82a769abcd0f58e8547e1fed90ae1093
Dcbaas <baasdav@eos01.cis.gvsu.edu>
2018-02-19 07:46:43 -0500

    Revised comments for balancedSymbols

--- src/functions.cpp
+++ src/functions.cpp
@@ -4,10 +4,31 @@
 #include <vector>
 #include <stack>
 
+
+//Author: David Baas
+//Version: 2/19/2018
+//Since: 2/13/2018
+
+/**********************************************************************
+ * The balancedSymbols function looks at a string and determines 
+ * weather or not there is symbol balance for the parentetical symbols:
+ * '()', '[]', and the '{}'. It accomplishes this by using a stack
+ * structture to track left parenthetical symbols. When a right 
+ * parenthetical symbol is come across, it is compared to the current 
+ * left parenthetical symbol on the top of the stack. If the stack is 
+ * empty, false is returned. If the symbols don't match, false is 
+ * returned. If they symbols match, the top symbol is popped and the 
+ * next char in the input string is assesed. If at the end of the
+ * input string the stack is empty, true is returned.
+ *
+ * @param input string that is the input being tested.
+ * @return true if the stack is empty at the end of the input string
+ * analysis. False if one of the reasons above occurs.
+ **********************************************************************/
   bool balancedSymbols(std::string input){
 	std::stack<char, std::vector<char> > input_stack;
 	for(auto input_it = input.begin(); input_it != input.end(); input_it++){
-		//Add a starting parenthese to the stack (, [, or {.
+		//Check the char to see if it matches a lef parethetical symbol.
 		switch(*input_it){
 			case '(':
 				input_stack.push(*input_it);
@@ -20,30 +41,35 @@
 			case '{':
 				input_stack.push(*input_it);
 				break;
-		
+		//Check if the char is a right parenthetical symbol.
 			case ')':
-				if(input_stack.empty())// is the stack empty. if yes then this is an error and there won't be balance
+				// Is the stack empty? if yes, then this is
+				//  an error and there won't be balance.
+				if(input_stack.empty())
 					return false;
-				else if(input_stack.top() == '('){ // Does the top match the current char symbol. if yes pop the top.
+				// Does the top match the current char symbol?
+				//  If yes, pop the top.
+				else if(input_stack.top() == '('){ 
 					input_stack.pop();
 					break;
 				}
+				//The symbols don't match return false.
 				else
 					return 0;
 	
 			case ']':
-				if(input_stack.empty())// is the stack empty. if yes then this is an error and there won't be balance
+				if(input_stack.empty())
 					return 0;
-				else if(input_stack.top() == '['){ // Does the top match the current char symbol. if yes pop the top.
+				else if(input_stack.top() == '['){ 
 					input_stack.pop();
 					break;
 				}
 				else
 					return 0;
 			case '}':
-				if(input_stack.empty())// is the stack empty. if yes then this is an error and there won't be balance  							
+				if(input_stack.empty())  							
                                		return 0;
-		        	else if(input_stack.top() == '{'){ // Does the top match the current char symbol. if yes pop the top.
+		        	else if(input_stack.top() == '{'){ 
 					input_stack.pop();
 					break;
 				}
@@ -51,7 +77,8 @@
 					return 0;
 		}
 	}		
-
+	//Is the stack empty after the string analysis?
+	//If yes, return true.
 	if(input_stack.empty())
 		return 1;
 	
@@ -59,6 +86,8 @@
   }
 
   std::string iToP(std::string input){
+	
+
 	return "String";
 
   }  
